68c915fe27f94a016b42c012e96d9a6ab8e2a3dd
caitzh <1262074036@qq.com>
2017-06-06 15:12:41 +0800

    增加用户密码,在数据库中加密存储

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
@@ -9,9 +10,21 @@ class User(db.Model):
 
     id = db.Column(db.String(11), doc='手机号码', primary_key=True)
     nickname = db.Column(db.String(20), doc='昵称', default='微聊用户', nullable=False)
-
+    password_hash = db.Column(db.String(128), doc='密码散列值', nullable=False)
+    
     def __repr__(self):
         return '%s <%s>' % (self.nickname, self.id)
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __json__(self):
         return {
--- app/router.py
+++ app/router.py
@@ -4,14 +4,17 @@ from models import db, User
 
 main = Blueprint('main', __name__)
 
-@main.route('/regesiter/<id>/<username>')
-def regesiter(id, username):
-    user = User(id=id, nickname=username)
+@main.route('/regesiter/<id>/<username>/<password>')
+def regesiter(id, username, password):
+    user = User(id=id, nickname=username, password=password)
     db.session.add(user)
     db.session.commit()
     return 'User %s' % username
 
-@main.route('/login/<id>')
-def login(id):
+@main.route('/login/<id>/<password>')
+def login(id, password):
     user = User.query.filter_by(id=id).first()
-    return 'User %s' % user.nickname
+    if user.verify_password(password):
+        return 'User %s' % user.nickname
+    else:
+        return 'wrong password'
